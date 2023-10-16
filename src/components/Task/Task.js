0e757e2557050c2d954eee71e15808296cc6ad7e
Vybornyi <1vybornyi@gmail.com>
2023-10-16 14:30:15 +0200

    ref add styles directly to Test element

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,55 @@
-import { postTaskData } from "../../gateway/index.js";
+import { postTaskData } from '../../gateway/index';
+import checkIcon from '../../images/check-lg.svg';
+import xIcon from '../../images/x-lg.svg';
+import arrRightIcon from '../../images/chevron-right.svg';
 
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <link rel="stylesheet" href="./src/components/Task/Task.css"/>
+   <style>
+    .task{
+      position: relative;
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      margin-bottom: 1.2em;
+      padding:   1rem 3rem 1rem 1rem ;
+      border: 3px solid #3D405B;
+      border-radius: 15px;
+      background-color: #F4F1DE;
+      cursor: pointer;
+    }
+
+    #check-icon{
+      height: 2.5rem;
+    }
+
+    .task__status-item{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-right: 1rem;
+      padding: 5px;
+      background-color: #E07A5F;
+      border-radius: 50%;
+      border: 2px solid #3D405B;
+      font-size: 3em;
+      color:aliceblue
+    }
+
+    .task__status-item_checked{
+      background-color: #81B29A;
+    }
+
+    p{
+      font-size: 1.2rem;
+    }
+
+    .task_arrow-icon{
+      position: absolute;
+      right: 1rem;
+    }
+  </style>
     <div class="task">
       <div id="status-item" class="task__status-item">
         <img id="check-icon"/>
@@ -11,16 +57,17 @@ template.innerHTML = `
       <div class="task__info-wrapper">
         <h2 class="task__title">
           <slot name="title"/>
+          
         </h2>
         <p class="task__description">
           <slot name="description"/>
         </p>
       </div>
-      <img class="task_arrow-icon" src="./src/icons/chevron-right.svg"/>
+      <img class="task_arrow-icon" src="${arrRightIcon}"/>
     </div>
-`
+`;
 
-export class TaskElement extends HTMLElement {
+export default class TaskElement extends HTMLElement {
   constructor() {
     super();
     this.id = null;
@@ -33,11 +80,14 @@ export class TaskElement extends HTMLElement {
 
     this.titleSlot = shadowRoot.querySelector('slot[name="title');
     this.descriptionSlot = shadowRoot.querySelector('slot[name="description"]');
-    this.statusItem = shadowRoot.getElementById("status-item");
-    this.checkIcon = shadowRoot.getElementById("check-icon");
+    this.statusItem = shadowRoot.getElementById('status-item');
+    this.checkIcon = shadowRoot.getElementById('check-icon');
   }
+
   loadData(tasksData, projectId) {
-    const { id, title, description, checked } = tasksData;
+    const {
+      id, title, description, checked,
+    } = tasksData;
     this.id = id;
     this.title = title;
     this.description = description;
@@ -51,7 +101,7 @@ export class TaskElement extends HTMLElement {
     const taskInfo = {
       projectId: this.projectId,
       taskId: this.id,
-      checked: this.checked
+      checked: this.checked,
     };
 
     postTaskData(taskInfo);
@@ -64,8 +114,8 @@ export class TaskElement extends HTMLElement {
   }
 
   updateUI() {
-    this.statusItem.className = this.checked ? "task__status-item task__status-item_checked" : "task__status-item";
-    this.checkIcon.src = this.checked ? "./src/icons/check-lg.svg" : "./src/icons/x-lg.svg";
+    this.statusItem.className = this.checked ? 'task__status-item task__status-item_checked' : 'task__status-item';
+    this.checkIcon.src = this.checked ? checkIcon : xIcon;
   }
 
   connectedCallback() {
@@ -77,6 +127,3 @@ export class TaskElement extends HTMLElement {
     this.removeEventListener('click', this.toggleStatus);
   }
 }
-
-window.customElements.define('task-element', TaskElement);
-
